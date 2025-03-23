fb2961f52f97cbec0629d9c5045df6633b822a3b
Pravesh532 <praveshpatel9569@gmail.com>
2025-03-23 14:38:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+ functio updateTime(){
+	 let time = new Date().toLocalString();
+	 document.getElementById("timer").textContent = time;
+ }
+setInterval(updateTime,1000);
+updateTime();
